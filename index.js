81ea20b6bc60cfecd17a2be192f8c08e69f0f567
Dustin Palmatier <dustinpalmatier@icloud.com>
2021-02-27 17:51:53 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,98 @@
+const { Client } = require('rustrcon');
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const config = require("./config");
+
+config.Servers.forEach((server, index) => {
+    server.name = `${server.IP}:${server.Port}/${index}`;
+    server.rcon = new Client({
+        ip: server.IP,
+        port: server.Port,
+        password: server.RconPassword
+    });
+    server.connected = false;
+    server.bot = new Discord.Client();
+    server.bot.login(server.DiscordToken);
+
+    server.bot.on('ready', () => {
+        console.log(`Successfully launched: ${server.bot.user.tag}`);
+        server.bot.user.setActivity("Attempting connection...");
+        attemptConnection();
+    });
+
+    server.rcon.on('connected', () => {
+        console.log(`Successfully connected to ${server.rcon.ws.ip}:${server.rcon.ws.port}`);
+
+        server.bot.user.setActivity("Monitoring chat...");
+    });
+
+    server.rcon.on('error', (err) => {
+    });
+
+    server.rcon.on('disconnect', () => {
+        server.bot.user.setActivity("Currently offline...");
+        attemptConnection();
+    });
+
+    server.rcon.on('message', async (message) => {
+        const mType = message["Type"];
+
+        switch (mType) {
+            case "Chat":
+                const mContent = message["content"];
+
+                const channel = mContent["Channel"];
+                const chatmessage = mContent["Message"];
+                const user = mContent["UserId"];
+                const username = mContent["Username"];
+                const time = new Date(mContent["Time"] * 1000);
+                const avatar = await fetchAvatarURI(user).catch(err => console.log(err));
+
+                const messageLog = new Discord.MessageEmbed()
+                    .setFooter(server.ServerIdentifier)
+                    .setColor("#CE412B")
+                    .setDescription(
+                        `[${cleanText(username)}](https://www.steamcommunity.com/profiles/${user}) - ${channel == 1 ? "Team" : "Global"} Chat\n\n` +
+                        cleanText(chatmessage)
+                    )
+                    .setTimestamp(time)
+                    .setThumbnail(avatar);
+
+                server.bot.guilds.fetch(server.DiscordServerGuildID)
+                        .then(guild => {
+                            guild.channels.cache.get(server.MessageLogChannel)
+                                .send({ disableMentions: "all" , embed: messageLog });
+                        })
+                        .catch(err => console.log(err));
+                    
+                break;
+        }
+    })
+
+    function attemptConnection() {
+        server.rcon.login();
+    }
+
+    function cleanText(text) {
+        let cleanedText = Discord.Util.escapeMarkdown(text);
+        cleanedText = Discord.Util.removeMentions(cleanedText);
+
+        return cleanedText;
+    }
+
+    function fetchAvatarURI(SteamID) {
+        return new Promise(async (resolve, reject) => {
+            const request = await fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${config.SteamAPIKey}&steamids=${SteamID}`)
+                .catch((err) => {
+                    return reject(err);
+                });
+
+            if (request.status !== 200)
+                return reject(request.statusText);
+            
+            const data = await request.json();
+            
+            return resolve(data.response.players[0]["avatar"]);
+        });
+    }
+});
---
+++ readme.md
@@ -0,0 +1,61 @@
+This utility will allow you to have your entire server chat dumped into a single channel on Discord. You can easily search through the messages with their Steam ID and the server it is on.  
+  
+# Requirements  
+- Your own Discord Bot  
+- Your own Steam API key  
+- Discord Server  
+- Access to RCON  
+- Node JS  
+  
+# Attention  
+You will need to have Node JS installed on the machine that will be running this! If you do not have that yet installed, then you may install it \[here\](https://nodejs.org/en/)!  
+  
+# Setup  
+1. Open config.yml with a file editor.  
+2. Visit https://discord.com/developers/applications  
+3. Login and then select "New Application" in the top right.  
+4. Once you created the application select the "Bot" tab and then select "Add Bot".  
+5. Click the "Copy" button under the section called "Token".  
+6. Paste the token in the config file where it says "DiscordToken".  
+7. Invite the bot to your Discord server.  
+7. Visit https://www.steamcommunity.com/dev/apikey  
+9. If you haven't created a API Key already then create one.  
+10. Copy the API key you were assigned and paste it in the config where it says "SteamAPIKey".  
+11. Open Discord and go to your setting.  
+12. Select appearance and scroll down until you see "Developer Mode" and enable it.  
+13. Find your Discord server in the server list and right click the icon and select "Copy ID".  
+14. Paste this ID inside of the config where it says "DiscordServerGuildID".  
+15. Right click the channel where you want the messages to go in your Discord and select "Copy ID".  
+16. Paste this ID inside of your config where it says "MessageLogChannel".  
+17. Ensure you have RCON enabled correctly (Learn more \[here\](https://www.rustafied.com/how-to-host-your-own-rust-server) by looking at launch options).  
+18. Inside of the config replace "IP" with the IP to your Rust server, "Port" with the port your RCON is on, and "RconPassword" with the password you set for RCON.  
+19. Save the config file.  
+20. Run start.bat  
+  
+# Configuration  
+```  
+{  
+  "INFORMATION": "You will need to obtain a Discord Bot Token (https://discord.com/developers/applications) and a Steam API Key (https://steamcommunity.com/dev/apikey) from the links provided!",  
+  "Servers": [  
+    {  
+      "IP": "IP ADDRESS",  
+      "Port": 28016,  
+      "RconPassword": "PASSWORD",  
+      "ServerIdentifier": "SERVER NAME",  
+      "DiscordToken": "BOT TOKEN",  
+      "DiscordServerGuildID": "GUILD IDENTIFIER",  
+      "MessageLogChannel": "CHANNEL IDENTIFIER"  
+    },  
+    {  
+      "IP": "IP ADDRESS",  
+      "Port": 28016,  
+      "RconPassword": "PASSWORD",  
+      "ServerIdentifier": "SERVER NAME",  
+      "DiscordToken": "BOT TOKEN",  
+      "DiscordServerGuildID": "GUILD IDENTIFIER",  
+      "MessageLogChannel": "CHANNEL IDENTIFIER"  
+    }  
+  ],  
+  "SteamAPIKey": "STEAM API KEY"  
+}  
+```
