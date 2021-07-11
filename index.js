26dc3c9830e7739597a1aaa37e40e74b4a8a66fd
Dustin Palmatier <dustinpalmatier@icloud.com>
2021-07-11 06:38:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,149 +1,211 @@
-const { Client } = require('rustrcon');
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
+const { Client } = require("rustrcon");
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
 const config = require("./config");
+let serverStatus = [];
 
 config.Servers.forEach((server, index) => {
-    server.name = `${server.IP}:${server.Port} (#${index + 1})`;
-    server.rcon = new Client({
-        ip: server.IP,
-        port: server.Port,
-        password: server.RconPassword
-    });
+  server.name = `${server.IP}:${server.Port} (#${index + 1})`;
+  server.rcon = new Client({
+    ip: server.IP,
+    port: server.Port,
+    password: server.RconPassword,
+  });
+  server.connected = false;
+  server.bot = new Discord.Client();
+  server.bot.login(server.DiscordToken);
+  server.playerCountUpdate = null;
+
+  server.scheduler = null;
+
+  server.bot.on("ready", () => {
+    console.log(`[${server.bot.user.tag}] Successfully launched...`);
+    attemptConnection();
+  });
+
+  server.rcon.on("connected", () => {
+    server.connected = true;
+
+    console.log(
+      `[${server.bot.user.tag}] Successfully connected to ${server.rcon.ws.ip}:${server.rcon.ws.port}`
+    );
+
+    const onlineEmbed = new Discord.MessageEmbed()
+      .setTitle("Server Status")
+      .setColor(server.EmbedColor)
+      .setDescription(`${server.ServerIdentifier} is now online!`)
+      .setTimestamp()
+      .setThumbnail(server.ServerLogo);
+
+    server.bot.guilds
+      .fetch(server.DiscordServerGuildID)
+      .then((guild) => {
+        guild.channels.cache
+          .get(server.StatusChannel)
+          .send({ disableMentions: "all", embed: onlineEmbed });
+      })
+      .catch((err) => console.log(err));
+
+    server.bot.user.setActivity("Fetching data...");
+
+    server.playerCountUpdate = setInterval(() => {
+      try {
+        server.rcon.send("serverinfo", "Reheight", 88724);
+      } catch (err) {
+        logError(err);
+      }
+    }, 20000);
+  });
+
+  server.rcon.on("error", (err) => {
+    logError(err.message);
+  });
+
+  function logError(err) {
+    console.log(
+      `[${server.bot.user.tag}] There was an issue while connecting to ${server.name}...\n\n---------[ERROR]---------\n\n${err}\n\n-------------------------\n`
+    );
+  }
+
+  server.rcon.on("disconnect", () => {
+    clearInterval(server.playerCountUpdate);
+
+    serverStatus[index] = { playersOnline: 0, queuedPlayers: 0 };
+
+    const offlineEmbed = new Discord.MessageEmbed()
+      .setTitle("Server Status")
+      .setColor(server.EmbedColor)
+      .setDescription(`${server.ServerIdentifier} has gone offline!`)
+      .setTimestamp()
+      .setThumbnail(server.ServerLogo);
+
+    if (server.connected)
+      server.bot.guilds
+        .fetch(server.DiscordServerGuildID)
+        .then((guild) => {
+          guild.channels.cache
+            .get(server.StatusChannel)
+            .send({ disableMentions: "all", embed: offlineEmbed });
+        })
+        .catch((err) => console.log(err));
+
     server.connected = false;
-    server.bot = new Discord.Client();
-    server.bot.login(server.DiscordToken);
-        server.playerCountUpdate = null;
 
-    server.scheduler = null;
+    server.bot.user.setActivity("Currently offline...");
 
-    server.bot.on('ready', () => {
-        console.log(`[${server.bot.user.tag}] Successfully launched...`);
-        attemptConnection();
-    });
+    console.log(
+      `[${server.bot.user.tag}] RCON disconnected from ${server.name}`
+    );
 
-    server.rcon.on('connected', () => {
-        server.connected = true;
+    attemptConnection();
+  });
 
-        console.log(`[${server.bot.user.tag}] Successfully connected to ${server.rcon.ws.ip}:${server.rcon.ws.port}`);
+  server.rcon.on("message", async (message) => {
+    const mType = message["Type"];
+    const mContent = message["content"];
+    const mIdentifier = message["Identifier"];
 
-        const onlineEmbed = new Discord.MessageEmbed()
-            .setTitle("Server Status")
-            .setColor(server.EmbedColor)
-            .setDescription(`${server.ServerIdentifier} is now online!`)
-            .setTimestamp()
-            .setThumbnail(server.ServerLogo);
+    switch (mType) {
+      case "Chat":
+        const channel = mContent["Channel"];
+        const chatmessage = mContent["Message"];
+        const user = mContent["UserId"];
+        const username = mContent["Username"];
+        const time = new Date(mContent["Time"] * 1000);
 
-        server.bot.guilds.fetch(server.DiscordServerGuildID)
-            .then(guild => {
-                guild.channels.cache.get(server.StatusChannel)
-                    .send({ disableMentions: "all" , embed: onlineEmbed });
-            })
-            .catch(err => console.log(err));
+        const messageLog = new Discord.MessageEmbed()
+          .setFooter(server.ServerIdentifier)
+          .setColor(server.EmbedColor)
+          .setDescription(
+            `[${cleanText(
+              username
+            )}](https://www.steamcommunity.com/profiles/${user}) - ${
+              channel == 1 ? "Team" : "Global"
+            } Chat\n\n` + cleanText(chatmessage)
+          )
+          .setTimestamp(time);
 
-        server.bot.user.setActivity("Fetching data...");
+        server.bot.guilds
+          .fetch(server.DiscordServerGuildID)
+          .then((guild) => {
+            guild.channels.cache
+              .get(server.MessageLogChannel)
+              .send({ disableMentions: "all", embed: messageLog });
+          })
+          .catch((err) => console.log(err));
 
-        server.playerCountUpdate = setInterval(() => {
-                        try {
-                                server.rcon.send("serverinfo", "Reheight", 88724);
-                        } catch (err) {
-                                logError(err);
-                        }
-        }, 20000);
-    });
+        break;
+      case "Generic":
+        if (mIdentifier === 88724) {
+          const playersOnline = mContent["Players"] + mContent["Joining"];
+          const maxPlayers = mContent["MaxPlayers"];
+          const queuedPlayers = mContent["Queued"];
 
-    server.rcon.on('error', (err) => {
-        logError(err.message);
-    });
+          serverStatus[index] = {
+            playersOnline,
+            queuedPlayers,
+          };
 
-        function logError(err) {
-                console.log(`[${server.bot.user.tag}] There was an issue while connecting to ${server.name}...\n\n---------[ERROR]---------\n\n${err}\n\n-------------------------\n`);
+          server.bot.user.setActivity(
+            `${playersOnline}/${maxPlayers} (${queuedPlayers})`,
+            {
+              type: "WATCHING",
+            }
+          );
         }
 
-    server.rcon.on('disconnect', () => {
-                clearInterval(server.playerCountUpdate);
-
-        const offlineEmbed = new Discord.MessageEmbed()
-            .setTitle("Server Status")
-            .setColor(server.EmbedColor)
-            .setDescription(`${server.ServerIdentifier} has gone offline!`)
-            .setTimestamp()
-            .setThumbnail(server.ServerLogo);
-
-        if (server.connected)
-            server.bot.guilds.fetch(server.DiscordServerGuildID)
-                .then(guild => {
-                    guild.channels.cache.get(server.StatusChannel)
-                        .send({ disableMentions: "all" , embed: offlineEmbed });
-                })
-                .catch(err => console.log(err));
-
-        server.connected = false;
-
-        server.bot.user.setActivity("Currently offline...");
-
-        console.log(`[${server.bot.user.tag}] RCON disconnected from ${server.name}`);
-
-        attemptConnection();
-    });
-
-    server.rcon.on('message', async (message) => {
-        const mType = message["Type"];
-        const mContent = message["content"];
-        const mIdentifier = message["Identifier"];
-
-        switch (mType) {
-            case "Chat":
-                const channel = mContent["Channel"];
-                const chatmessage = mContent["Message"];
-                const user = mContent["UserId"];
-                const username = mContent["Username"];
-                const time = new Date(mContent["Time"] * 1000);
-
-                const messageLog = new Discord.MessageEmbed()
-                    .setFooter(server.ServerIdentifier)
-                    .setColor(server.EmbedColor)
-                    .setDescription(
-                        `[${cleanText(username)}](https://www.steamcommunity.com/profiles/${user}) - ${channel == 1 ? "Team" : "Global"} Chat\n\n` +
-                        cleanText(chatmessage)
-                    )
-                    .setTimestamp(time)
-
-                server.bot.guilds.fetch(server.DiscordServerGuildID)
-                        .then(guild => {
-                            guild.channels.cache.get(server.MessageLogChannel)
-                                .send({ disableMentions: "all" , embed: messageLog });
-                        })
-                        .catch(err => console.log(err));
-
-                break;
-            case "Generic":
-                if (mIdentifier === 88724) {
-                    const playersOnline = mContent["Players"] + mContent["Joining"];
-                    const maxPlayers = mContent["MaxPlayers"];
-                    const queuedPlayers = mContent["Queued"];
-
-                    server.bot.user.setActivity(
-                        `${playersOnline}/${maxPlayers} (${queuedPlayers})`,
-                        {
-                            type: "PLAYING"
-                        }
-                    )
-                }
-
-                break;
-        }
-    });
-
-    function attemptConnection() {
-        console.log(`[${server.bot.user.tag}] Attempting to connect to ${server.name}`);
-        server.rcon.login();
+        break;
     }
+  });
 
-    function cleanText(text) {
-        let cleanedText = Discord.Util.escapeMarkdown(text);
-        cleanedText = Discord.Util.removeMentions(cleanedText);
+  function attemptConnection() {
+    console.log(
+      `[${server.bot.user.tag}] Attempting to connect to ${server.name}`
+    );
+    server.rcon.login();
+  }
 
-        return cleanedText;
-    }
+  function cleanText(text) {
+    let cleanedText = Discord.Util.escapeMarkdown(text);
+    cleanedText = Discord.Util.removeMentions(cleanedText);
+
+    return cleanedText;
+  }
 });
+
+async function getTotalPopulation() {
+  return new Promise((res, rej) => {
+    if (serverStatus.length === 0) rej("No servers yet exist in the array!");
+
+    let totalData = { playersOnline: 0, playersQueued: 0 };
+
+    serverStatus.forEach(({ playersOnline, queuedPlayers }, index, array) => {
+      totalData.playersOnline += playersOnline;
+      totalData.playersQueued += queuedPlayers;
+
+      if (index === array.length - 1) res(totalData);
+    });
+  });
+}
+
+if (!config.MasterBot || !config.MasterBot.DiscordToken)
+    return;
+
+const totalPopulationBot = new Discord.Client();
+
+totalPopulationBot.on("ready", () => {
+  setInterval(async () => {
+    const totalPop = await getTotalPopulation().catch(() => null);
+
+    if (!totalPop || totalPop == null) {
+      return totalPopulationBot.user.setActivity("Fetching data...");
+    }
+
+    totalPopulationBot.user.setActivity(
+      `${totalPop.playersOnline} (${totalPop.playersQueued})`,
+      { type: "WATCHING" }
+    );
+  }, 20000);
+});
+
+totalPopulationBot.login(config.MasterBot.DiscordToken);
